d463a12af43987bd1cfe31084becd011ae483397
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-08 22:39:53 +0600

    port changed

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 9000;
 
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('This is User Management Server')
+app.use(cors());
+app.get( '/', (req, res) => {
+    res.send('Ready To Contribute upcoming Full Stack Project')
 })
-
-
 app.listen( port , () => {
-    console.log(`Our Server is running on the port: ${port}`);
+    console.log(`Our Awesome server in running on the port: ${port}`);
 })
