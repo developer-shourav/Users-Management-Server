876f96f18545138e66454d4c69966cacde5dccdd
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-08 22:53:07 +0600

    user route and user data added

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const user = [
+    {id:1, name:'Akibul', age:45, education:'Dhaka University'},
+    {id:2, name:'MokBul', age:45, education:'Dhaka University'},
+    {id:3, name:'Robiul', age:45, education:'Dhaka University'},
+    {id:4, name:'Dak Kno', age:45, education:'Dhaka University'},
+    {id:5, name:'Firoz', age:45, education:'Dhaka University'}
+];
+
+
+
 const cors = require('cors');
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('I am Ready To Make Awesome Server')
+})
+
+app.get('/user', ( req, res) => {
+    res.send(user)
 })
 
 app.listen( port, () => {
