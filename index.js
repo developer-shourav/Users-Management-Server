750eef41a6cb9a74d4bbd5ee9df7c6f38270cc01
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-08 22:34:19 +0600

    root route added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000 ;
+
 const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('This is User Management Server')
+})
+
+
+app.listen( port , () => {
+    console.log(`Our Server is running on the port: ${port}`);
+})
