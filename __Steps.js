27a496dcdbb69a5f6444448c19453ad3baf9288f
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-09 16:35:13 +0600

    steps of POST method added

--- __Steps.js
+++ __Steps.js
@@ -1,5 +1,12 @@
 /**
  * 1. Create A POST API in Server Side
  * 2. Send Data via POST From Client Side
- * 3.  
+ * 3. Set Fetch method inside the fetch options ( second parameter)
+ * 4. Option will have three things : 1. method 2. headers 3. body
+ *    method:'POST',
+ *    headers: {
+ *      'content-type': 'application/json'
+ *    },
+ *    body: JSON.stringify(data/value)
+ * 5. Don't forget to send data by JSON.stringify in the body. 
 */
