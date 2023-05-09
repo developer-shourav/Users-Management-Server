6a6c984b939097db865924f10c8efd7cd862eb9e
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-10 01:25:06 +0600

    servr post complete

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const users = [
-    {id:1, name:'Akibul', age:45, education:'Dhaka University'},
-    {id:2, name:'MokBul', age:45, education:'Dhaka University'},
-    {id:3, name:'Robiul', age:45, education:'Dhaka University'},
-    {id:4, name:'Dak Kno', age:45, education:'Dhaka University'},
-    {id:5, name:'Firoz', age:45, education:'Dhaka University'}, 
+    {id:1, name:'Akibul', age:45},
+    {id:2, name:'MokBul', age:45},
+    {id:3, name:'Robiul', age:45},
+    {id:4, name:'Dak Kno', age:45},
+    {id:5, name:'Firoz', age:45}, 
 ];
 
 
@@ -19,6 +19,7 @@ const users = [
 app.use(cors())
 app.use(express.json())
 
+
 app.get('/', (req, res) => {
     res.send('I am Ready To Make Awesome Server')
 });
@@ -28,10 +29,11 @@ app.get('/users', ( req, res) => {
 });
 
 app.post('/users', (req, res) => {
-    console.log('POST API Hitting');
-    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1 ;
+    users.push(newUser)
+    console.log(users);
 })
-
 
 app.listen( port, () => {
     console.log(`Our Server is running On the PORT: ${port}`);
